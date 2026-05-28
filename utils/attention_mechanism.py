af3f7af571bea03f076ac56b672e9d3add752382
Mongosaurusrex <nathan.dygant@gmail.com>
2026-05-28 20:21:22 +0200

    Simple attention mechanism

---
+++ utils/attention_mechanism.py
@@ -0,0 +1,13 @@
+import torch
+
+inputs = torch.tensor(
+    [[0.43, 0.15, 0.89],
+     [0.55, 0.87, 0.66],
+     [0.57, 0.85, 0.64],
+     [0.22, 0.58, 0.33],
+     [0.77, 0.25, 0.10],
+     [0.05, 0.80, 0.55]]
+)
+
+context_vect = torch.softmax(inputs @ inputs.T, dim=-1) @ inputs
+print(context_vect)
